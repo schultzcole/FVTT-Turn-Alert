40e113e5beeb05f65fa4b837aac279a4e4b5145c
Cole Schultz <schultzcole@gmail.com>
2020-07-14 23:17:30 -0500

    Add combat update hook listener which checks if any notifications should be triggered

--- turn-notification.js
+++ turn-notification.js
@@ -1,5 +1,29 @@
 import TurnNotificationManager from "./scripts/TurnNotificationManager.js";
+import CONST from "./scripts/const.js";
+import TurnNotification from "./scripts/TurnNotification.js";
 
 Hooks.on("init", () => {
     game.TurnNotificationManager = TurnNotificationManager;
-})
+});
+
+Hooks.on("updateCombat", (combat, changed, diff, userId) => {
+    if (!("round" in changed || "turn" in changed)) return;
+
+    const notifications = combat.getFlag(CONST.moduleName, "notifications");
+
+    const turn = combat.turns[combat.data.turn];
+    const round = combat.data.round;
+    let anyDeleted = false;
+    for (let id in notifications) {
+        const notification = notifications[id];
+        if (TurnNotification.checkTrigger(notification, round, "round" in changed, turn)) {
+            console.log(`Triggered! ${notification.message}`);
+            if (!notification.repeating) {
+                delete notifications[id];
+                anyDeleted = true;
+            }
+        }
+    }
+
+    if (anyDeleted) combat.setFlag(CONST.moduleName, "notifications", notifications);
+});
