89e48a8223f1102032927c6c2ac86aae56a9294e
Cole Schultz <schultzcole@gmail.com>
2020-07-16 16:58:49 -0500

    Make turn notification manager make more sense

--- scripts/TurnNotificationManager.js
+++ scripts/TurnNotificationManager.js
@@ -1,65 +1,67 @@
 import TurnNotificationConfig from "../apps/TurnNotificationConfig.js";
 import CONST from "./const.js";
-
-/**
- * Displays the dialog for creating a new Turn Notification which will activate on the current turn.
- *
- * The Turn Notification will be added to the active combat.
- * @param {Object} data Initial data for the resulting turn notification.
- * @param {Object} options Extra options for the TurnNotificationConfig FormApplication
- */
-function createOnCurrentTurn(data, options) {
-    const turnId = game.combat.turns?.length ? game.combat.turns[game.combat.data.turn]._id : null;
-
-    _create(mergeObject(data, { turn: turnId }), options);
-}
-
-/**
- * Displays the dialog for creating a new Turn Notification which will
- * activate at the top of a round rather than a particular turn.
- *
- * The Turn Notification will be added to the active combat.
- * @param {Object} data Initial data for the resulting turn notification.
- * @param {Object} options Extra options for the TurnNotificationConfig FormApplication
- */
-function createAtTopOfRound(data, options) {
-    _create(data, options);
-}
-
-function _create(data, options) {
-    if (!game.combat) {
-        ui.notifications.error("Cannot create a turn notification if no combats exist.");
-        return;
-    }
-
-    const currentCombat = game.combat.data;
-
-    const notificationData = {
-        combat: currentCombat._id,
-        createdRound: currentCombat.round,
-        round: data.roundAbsolute ? currentCombat.round + 1 : 1,
-        user: game.userId,
-    };
-
-    const app = new TurnNotificationConfig(mergeObject(notificationData, data), options);
-    app.render(true);
-}
-
-/**
- * Clears all notifications on a given combat.
- * @param {String} combatId The id string of the combat to clear all notifications for.
- */
-async function clearAll(combatId) {
-    const combat = combatId ? game.combats.get(combatId) : game.combat;
-    return combat.unsetFlag(CONST.moduleName, "notifications");
-}
+import CombatNotificationApplication from "../apps/CombatNotificationApplication.js";
 
 /**
  * Easily accessible utility functions for Turn Notifications
  */
-let TurnNotificationManager = {
-    createOnCurrentTurn: createOnCurrentTurn,
-    createAtTopOfRound: createAtTopOfRound,
-    clearAll: clearAll,
-};
-export default TurnNotificationManager;
+export default class TurnNotificationManager {
+    /**
+     * Displays the dialog for creating a new Turn Notification which will activate on the current turn.
+     *
+     * The Turn Notification will be added to the active combat.
+     * @param {Object} data Initial data for the resulting turn notification.
+     * @param {Object} options Extra options for the TurnNotificationConfig FormApplication
+     */
+    createOnCurrentTurn(data, options) {
+        const turnId = game.combat.turns?.length ? game.combat.turns[game.combat.data.turn]._id : null;
+
+        this._create(mergeObject(data, { turn: turnId }), options);
+    }
+
+    /**
+     * Displays the dialog for creating a new Turn Notification which will
+     * activate at the top of a round rather than a particular turn.
+     *
+     * The Turn Notification will be added to the active combat.
+     * @param {Object} data Initial data for the resulting turn notification.
+     * @param {Object} options Extra options for the TurnNotificationConfig FormApplication
+     */
+    createAtTopOfRound(data, options) {
+        this._create(data, options);
+    }
+
+    _create(data, options) {
+        if (!game.combat) {
+            ui.notifications.error("Cannot create a turn notification if no combats exist.");
+            return;
+        }
+
+        const currentCombat = game.combat.data;
+
+        const notificationData = {
+            combat: currentCombat._id,
+            createdRound: currentCombat.round,
+            round: data.roundAbsolute ? currentCombat.round + 1 : 1,
+            user: game.userId,
+        };
+
+        const app = new TurnNotificationConfig(mergeObject(notificationData, data), options);
+        app.render(true);
+    }
+
+    /**
+     * Clears all notifications on a given combat.
+     * @param {String} combatId The id string of the combat to clear all notifications for.
+     */
+    async clearAllNotificationsForCombat(combatId) {
+        const combat = combatId ? game.combats.get(combatId) : game.combat;
+        return combat.unsetFlag(CONST.moduleName, "notifications");
+    }
+
+    viewNotificationsForCombat(combatId) {
+        if (!combatId) combatId = game.combat.data._id;
+        const app = new CombatNotificationApplication({ combatId });
+        app.render(true);
+    }
+}
